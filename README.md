a5c0e692fcda1851984d87c00d197a23d5bc1b68
Szabó Csaba <szabo.csaba.1981@gmail.com>
2021-04-08 12:29:29 +0200

    grammary correction

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # API query from https://www.thecocktaildb.com/
+
+Query coctails with the starting letter. U Built with HTML, JS, Bootstrap
+
+## Hosting
+
+[oksimano.github.io/API.cocktails](https://oksimnano.github.io/API-cocktails)
--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -24,7 +24,7 @@ document.querySelector("form").addEventListener("submit", (event) => {
 	fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${option}`)
 		.then((result) => result.json()) // from result makes a json
 		.then((data) => {
-			const cocktailList = data.drinks; //Array from API contains cocktail with choosing letter
+			const cocktailList = data.drinks; //Array from API contains cocktail with choosing a letter
 			const markup = cocktailList // Make a markup
 				.map(
 					(cocktail) => `
